1ae03502a5c3c8796edd313370a40e5f75621462
Taras Bohoslavets <bogoslavetstaras@gmail.com>
2023-08-05 23:32:17 +0300

    lessons10

--- src/lessons9/App.java
+++ src/lessons9/App.java
@@ -1,8 +1,35 @@
 package lessons9;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 public class App {
     public static void main ( String[] args ) {
-        System.out.println ("hello" );
+//
+        int[] arr = new int[5];
+        int[] arr2 = new int[]{1, 2, 3, 4, 5};
+        int[] arr3 = {1, 2, 3, 4, 5};
+//        System.out.println ( arr3 );
+//        System.out.println ( Arrays.toString ( arr3 ) );
+//        System.out.println ( arr3[1] );
+//        arr3[3] = 56;
+//        System.out.println ( Arrays.toString ( arr3 ) );
+//        arr3[2] = Integer.parseInt ( null );
+
+//        int[] arr5 = new int[5];
+//        arr5[4] = 15;
+//        System.out.println (arr5.length );
+//        System.out.println (arr5[arr5.length-1] );
+//        System.out.println (Arrays.toString ( arr5 ) );
+
+//        int[][] matrix = new int[4][3];
+//        matrix[1][1] = 11;
+//        System.out.println (Arrays.deepToString ( matrix ) );
+
+
+        int[][][] arr6 = new int[3][3][3];
+        System.out.println ( Arrays.deepToString ( arr6 ) );
+
 
 
     }
